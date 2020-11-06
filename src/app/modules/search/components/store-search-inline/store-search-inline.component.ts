22ff8a3cdcb0df936cbb029109c0d2df112d24c8
Ganesh Palankar <ganesh.p@sanradiance.com>
2020-11-06 10:57:35 +0530

    popover in progress

---
+++ src/app/modules/search/components/store-search-inline/store-search-inline.component.ts
@@ -0,0 +1,83 @@
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
+import { AfterViewInit, Component, ElementRef, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { fromEvent, interval } from 'rxjs';
+import { debounce, distinctUntilChanged, finalize, map, switchMap, tap } from 'rxjs/operators';
+import { ComponentPopoverRef, PopoverRef } from 'src/app/core/model/popover';
+import { RestApiService } from 'src/app/core/services/rest-api.service';
+import { SearchPanelComponent } from '../search-panel/search-panel.component';
+
+@Component({
+  selector: 'app-store-search-inline',
+  templateUrl: './store-search-inline.component.html',
+  styleUrls: ['./store-search-inline.component.scss']
+})
+export class StoreSearchInlineComponent implements AfterViewInit {
+  @ViewChild('searchInput', { read: ElementRef }) searchInput: ElementRef
+  @ViewChild('searchContainer', { read: ElementRef }) searchContainer: ElementRef
+  keyupSubs: any;
+  loading: boolean;
+  searchData: any;
+  overlayOpen: boolean;
+  // createdComponent: SearchPanelComponent;
+  // overlayRef: OverlayRef;
+
+  popoverRef: ComponentPopoverRef<SearchPanelComponent>;
+
+  constructor(private restApiService: RestApiService, private overlay: Overlay,
+    private vCRef: ViewContainerRef, private injector: Injector) { }
+
+  ngAfterViewInit(): void {
+    this.keyupSubs = fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        map((event: any) => event.target.value),
+        distinctUntilChanged(),
+        tap((term) => {
+          this.loading = true;
+          if (this.overlayOpen) this.popoverRef.instance.searchTerm = term;
+        }),
+        debounce(() => interval(1000)),
+        switchMap((val) => this.restApiService.get(`api/stores/search?name=${val}`).pipe(finalize(() => this.loading = false))),
+        map(resp => resp.data.stores || [])
+        // ).subscribe(res => this.searchData = res);
+      ).subscribe(res => { this.showResults(res); this.searchData = res });
+  }
+
+  showResults(results) {
+    if (this.overlayOpen) this.popoverRef.instance.results = results;
+    else this.openComponentPopover(results)
+  }
+
+  openComponentPopover(results = null) {
+    if (this.overlayOpen) return;
+
+    let config = new OverlayConfig();
+    config.hasBackdrop = false;
+    config.panelClass = 'sample-panel-panel';
+    config.positionStrategy = this.overlay.position().flexibleConnectedTo(this.searchContainer.nativeElement).withPositions([{
+      originX: 'end',
+      originY: 'bottom',
+      overlayX: 'end',
+      overlayY: 'top',
+    }]).withPush(false);
+
+    let overlayRef = this.overlay.create(config);
+    this.popoverRef = new ComponentPopoverRef<SearchPanelComponent>(overlayRef, null);
+    let compPortal = new ComponentPortal(SearchPanelComponent, null, this.createInjector(this.popoverRef, this.injector));
+    this.popoverRef.instance = overlayRef.attach(compPortal).instance;
+    this.popoverRef.onDismiss = () => { this.overlayOpen = false; }
+    this.overlayOpen = true;
+    this.popoverRef.instance.results = results;
+  }
+
+  closePopover() {
+    this.popoverRef.dismiss();
+    this.overlayOpen = false;
+  }
+
+  createInjector(popoverRef: PopoverRef, injector: Injector) {
+    const tokens = new WeakMap([[PopoverRef, popoverRef]]);
+    return new PortalInjector(injector, tokens);
+  }
+}
+
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -5,6 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoresModule } from '../stores/stores.module';
 import { StoreSearchComponent } from './components/store-search/store-search.component';
 import { IncrementalSearchModule } from '../incremental-search/incremental-search.module';
+import { StoreSearchInlineComponent } from './components/store-search-inline/store-search-inline.component';
+import { SearchPanelComponent } from './components/search-panel/search-panel.component';
 
 const routes: Routes = [
   {
@@ -15,12 +17,13 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [SearchComponent, StoreSearchComponent],
+  declarations: [SearchComponent, StoreSearchComponent, StoreSearchInlineComponent, SearchPanelComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     StoresModule,
     IncrementalSearchModule
-  ]
+  ],
+  exports: [StoreSearchInlineComponent]
 })
 export class SearchModule { }
