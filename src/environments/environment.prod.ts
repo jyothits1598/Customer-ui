7c260ec2db268e4a3fb6dc084078b15f25bbb4e9
Basavakumar <vishwanatha.st@sanradiance.com>
2020-11-12 15:57:48 +0530

    ui changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
 };
 
 /* Test server */
-export const API_URL_LINK = 'http://54.252.119.115:8000/';
+// export const API_URL_LINK = 'http://54.252.119.115:8000/';
 
-export const APP_LINK = 'http://54.252.119.115/';
+// export const APP_LINK = 'http://54.252.119.115/';
 
 /* UAT server */
-// export const API_URL_LINK = 'https://uat.api.menuzapp.com/';
+export const API_URL_LINK = 'https://uat.api.menuzapp.com/';
 
-// export const APP_LINK = 'https://uat.menuzapp.com.au/';
+export const APP_LINK = 'https://uat.menuzapp.com.au/';
