ee153e76c7979f5a4b5adf2dd8f48f4a17fbde98
Ganesh Palankar <ganesh.p@sanradiance.com>
2020-12-09 16:21:14 +0530

    future availability in store card

--- src/app/modules/time-availability/next-availability/next-availability.component.ts
+++ src/app/modules/time-availability/next-availability/next-availability.component.ts
@@ -9,7 +9,9 @@ import { TimeAvailability } from '../model/time-availability';
 export class NextAvailabilityComponent implements OnInit {
   @Input() availability: Array<TimeAvailability>;
   nextAvailability: TimeAvailability;
-
+  get dayDiff(): number {
+    return this.weekDayToNumber(this.nextAvailability.day) - this.now.getDay();
+  }
   now = new Date();
 
   constructor() { }
@@ -17,15 +19,15 @@ export class NextAvailabilityComponent implements OnInit {
   ngOnInit(): void {
     if (this.availability.length) {
       for (let i = 0; i < this.availability.length; i++) {
+
         let todayDay = this.now.getDay();
         if (todayDay === this.weekDayToNumber(this.availability[i].day)) {
-          let startTime = new Date();
-          startTime.setHours(this.gethours(this.availability[i].startTime), parseInt(this.availability[i].startTime.substr(3, 2)), 0);
-          if (this.now > startTime) { this.nextAvailability = this.availability[i]; break; }
+          let endTime = new Date();
+          endTime.setHours(this.gethours(this.availability[i].endTime), parseInt(this.availability[i].endTime.substr(3, 2)), 0);
+          if (endTime > this.now) { this.nextAvailability = this.availability[i]; break; }
         }
         if (todayDay < this.weekDayToNumber(this.availability[i].day)) { this.nextAvailability = this.availability[i]; break; }
       }
-
       if (!this.nextAvailability) this.nextAvailability = this.availability[0];
     }
   }
