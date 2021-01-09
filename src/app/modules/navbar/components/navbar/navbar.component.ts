6d71fbea7d9c196b31e416755e2a7739bdf24218
Ganesh S Palankar <ganesh.p@sanradiance.com>
2021-01-09 10:12:47 +0530

    search on in the home screen

--- src/app/modules/navbar/components/navbar/navbar.component.ts
+++ src/app/modules/navbar/components/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
 import { Router } from '@angular/router';
 import { ModalService } from 'src/app/core/services/modal.service';
 import { PopoverService } from 'src/app/core/services/popover.service';
+import { LayoutService } from 'src/app/core/services/layout.service';
 
 
 @Component({
@@ -13,10 +14,18 @@ import { PopoverService } from 'src/app/core/services/popover.service';
 })
 export class NavbarComponent implements OnInit {
   isLoggedin$: Observable<boolean>;
-  constructor(private authService: AuthService,private router: Router,) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private layoutService: LayoutService) { }
 
   ngOnInit(): void {
+    console.log('this.router , ', this.router.url);
     this.isLoggedin$ = this.authService.isLoggedIn$();
+  }
+
+  ShouldShoWSearch(){
+    return !(this.router.url !== '/' && this.layoutService.isMobile);
   }
 
   handleBlur() {
