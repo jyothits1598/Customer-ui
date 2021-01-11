a22d85448b15406c8a430aae0ea7bcbe38137c51
Basavakumar <vishwanatha.st@sanradiance.com>
2021-01-11 10:14:27 +0530

    ui changes

--- src/app/modules/favourite-stores/favourite-stores.module.ts
+++ src/app/modules/favourite-stores/favourite-stores.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FavouriteStoresComponent } from './pages/favourite-stores/favourite-stores.component';
 import { RouterModule, Routes } from '@angular/router';
 import { StoresModule } from '../stores/stores.module';
+import { NavbarModifierModule } from '../navbar-modifier/navbar-modifier.module';
 
 const routes: Routes = [
   {
@@ -16,7 +17,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    StoresModule
+    StoresModule,
+    NavbarModifierModule
   ]
 })
 export class FavouriteStoresModule { }
