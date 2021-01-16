d39a0671c8c28b0b1a8214bf281d13b155d4f954
Shaun <shaun.campbell.home@gmail.com>
2021-01-16 14:25:41 +1000

    cleanup

--- src/app/modules/navbar/components/navbar/navbar.component.ts
+++ src/app/modules/navbar/components/navbar/navbar.component.ts
@@ -1,4 +1,14 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+  ViewRef,
+} from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Router } from '@angular/router';
@@ -7,11 +17,10 @@ import { PopoverService } from 'src/app/core/services/popover.service';
 import { LayoutService } from 'src/app/core/services/layout.service';
 import { NavbarService } from '../../services/navbar.service';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
   isLoggedin$: Observable<boolean>;
@@ -20,8 +29,10 @@ export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
   //view reference will be stored only when the location selector view has been detached
   locationViewRef: ViewRef;
 
-  @ViewChild('locationTemp', { read: TemplateRef }) locationTemplate: TemplateRef<any>;
-  @ViewChild('vCont', { read: ViewContainerRef }) containerRef: ViewContainerRef;
+  @ViewChild('locationTemp', { read: TemplateRef })
+  locationTemplate: TemplateRef<any>;
+  @ViewChild('vCont', { read: ViewContainerRef })
+  containerRef: ViewContainerRef;
 
   templateSubs: Subscription;
 
@@ -29,13 +40,15 @@ export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
     private authService: AuthService,
     private router: Router,
     private layoutService: LayoutService,
-    private navbarService: NavbarService) { }
+    private navbarService: NavbarService
+  ) {}
 
   ngOnInit(): void {
     this.isLoggedin$ = this.authService.isLoggedIn$();
   }
 
   ngAfterViewInit(): void {
+    /*
     this.containerRef.createEmbeddedView(this.locationTemplate)
     this.templateSubs = this.navbarService.headingTemplate$.subscribe(
       (temp) => {
@@ -50,12 +63,16 @@ export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
           }
         }
       }
-    );
+    );*/
   }
 
   debug() {
-    if (this.locationViewRef) { this.containerRef.insert(this.locationViewRef); this.locationViewRef = null; }
-    else { this.locationViewRef = this.containerRef.detach(); }
+    if (this.locationViewRef) {
+      this.containerRef.insert(this.locationViewRef);
+      this.locationViewRef = null;
+    } else {
+      this.locationViewRef = this.containerRef.detach();
+    }
   }
 
   ShouldShoWSearch() {
@@ -65,5 +82,4 @@ export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy(): void {
     this.templateSubs.unsubscribe();
   }
-
 }
