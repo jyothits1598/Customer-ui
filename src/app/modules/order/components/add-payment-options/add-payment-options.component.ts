c2ae3a1a93531dddedbb2dd39adbf2f70e932a34
arunkumartc <arunkumar.tc@sanradiance.com>
2021-02-03 12:31:35 +0530

    add card popup

--- src/app/modules/order/components/add-payment-options/add-payment-options.component.ts
+++ src/app/modules/order/components/add-payment-options/add-payment-options.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
 import { OrderPages, OrderViewControllerService } from 'src/app/core/services/order-view-controller.service';
-
+import { ComponentModalRef, ModalRef } from 'src/app/core/model/modal';
+import { ModalService } from 'src/app/core/services/modal.service';
 @Component({
   selector: 'add-payment-options',
   templateUrl: './add-payment-options.component.html',
   styleUrls: ['./add-payment-options.component.scss']
 })
 export class AddPaymentOptionsComponent implements OnInit {
-
-  constructor(private orderView: OrderViewControllerService) { }
+  modalRef: ModalRef;
+  constructor(private orderView: OrderViewControllerService,
+    private modalService: ModalService,
+    ) { }
 
   ngOnInit(): void {
   }
 
   goBack() {
     this.orderView.showPage(OrderPages.CartSummary);
+  }
+  showModal(temp: TemplateRef<any>) {
+    this.modalRef = this.modalService.openTemplateModal(temp);
   }
 
 }
