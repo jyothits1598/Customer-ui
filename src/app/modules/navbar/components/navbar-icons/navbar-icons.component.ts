0674b2e705f1d6d1e3c98742ef4adc318f1dde17
Ganesh S Palankar <ganesh.p@sanradiance.com>
2021-02-09 16:49:45 +0530

    navbar icons active behaviour

--- src/app/modules/navbar/components/navbar-icons/navbar-icons.component.ts
+++ src/app/modules/navbar/components/navbar-icons/navbar-icons.component.ts
@@ -3,6 +3,7 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { DOCUMENT } from '@angular/common';
+import { filter, map } from 'rxjs/operators';
 
 
 @Component({
@@ -12,34 +13,35 @@ import { DOCUMENT } from '@angular/common';
 })
 export class NavbarIconsComponent implements OnInit, OnDestroy {
   isLoggedin: boolean;
-  isActive:string = '';
+  isActive: string = '';
   windowScrolled: boolean;
+
+  atHome$: Observable<boolean>
   constructor(
     private authService: AuthService,
-    private route: ActivatedRoute,@Inject(DOCUMENT) private document: Document) { 
-      // if(this.route.snapshot.queryParams['favourites']){
-      //   this.isActive = this.route.snapshot.queryParams['favourites'];
-      //   console.log(this.isActive);
-      // }
+    private router: Router, @Inject(DOCUMENT) private document: Document) {
+    this.atHome$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map((e: NavigationEnd) => e.url === '/'))
   }
 
   stateSubs: Subscription;
-  
+
   ngOnInit(): void {
     this.stateSubs = this.authService.isLoggedIn$().subscribe(state => this.isLoggedin = state);
   }
-  
+
   ngOnDestroy(): void {
     this.stateSubs.unsubscribe();
   }
 
-scrollToTop() {
+  scrollToTop() {
     (function smoothscroll() {
-        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-        if (currentScroll > 0) {
-            window.requestAnimationFrame(smoothscroll);
-            window.scrollTo(0, currentScroll - (currentScroll / 8));
-        }
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
     })();
-}
+  }
 }
