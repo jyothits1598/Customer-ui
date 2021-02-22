ca807567d4ec9fcdded97aa8af1c69705769dafb
arunkumartc <arunkumar.tc@sanradiance.com>
2021-02-22 21:44:26 +0530

    code menus updated

--- src/api/store-data.ts
+++ src/api/store-data.ts
@@ -4,7 +4,7 @@ export const URL_SetFavourite = 'api/stores/customer/favourite/add';
 export const URL_DeleteFavourite = 'api/stores/customer/favourite/delete';
 
 export function URL_StoreDetails(id: number) {
-    return `api/customer/stores/${id}/storedata`;
+    return `api/v1/stores/${id}`;
 }
 
 export function URL_StoreItemDetail(storeId: number, itemId: number) {
