80f6fb48d4c4ade264303ebedee6aad3454a4940
Ganesh S Palankar <ganesh.p@sanradiance.com>
2021-02-23 10:33:24 +0530

    social auth new resp

--- src/app/modules/authentication/services/social-auth-helper.service.ts
+++ src/app/modules/authentication/services/social-auth-helper.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { GoogleLoginProvider, SocialAuthService } from "angularx-social-login";
 import { FacebookLoginProvider } from "angularx-social-login";
 import { Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable()
 export class SocialAuthHelperService {
@@ -12,32 +13,42 @@ export class SocialAuthHelperService {
     this.hasLoaded = this.authService.initState;
   }
 
-  facebookSignIn(): Observable<{ email: string, token: string }> {
+  facebookSignIn(): Observable<{ email: string, token: string, firstName: string, lastName: string, type: string }> {
     this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
     return this.authService.authState.pipe(
       take(1),
       map((resp) => {
         let result = {
           token: resp.authToken,
-          email: resp.email
+          email: resp.email,
+          firstName: resp.firstName,
+          lastName: resp.lastName,
+          type: 'facebook'
         }
         return result;
       })
     );
   }
 
-  googleSignIn(): Observable<{ email: string, token: string }> {
+  googleSignIn(): Observable<{ email: string, token: string, firstName: string, lastName: string, type: string }> {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
     return this.authService.authState.pipe(
       take(1),
       map((resp) => {
         let result = {
           token: resp.idToken,
-          email: resp.email
+          email: resp.email,
+          firstName: resp.firstName,
+          lastName: resp.lastName,
+          type: 'google'
         }
         return result;
       })
     );
+  }
+
+  logout(){
+    this.authService.signOut();
   }
 
   // getAuthState(type: 'facebook' | 'google') {
