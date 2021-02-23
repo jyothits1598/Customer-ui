781fd7c56f0255076e1aa671fce226e9b9e66247
arunkumartc <arunkumar.tc@sanradiance.com>
2021-02-23 11:22:05 +0530

    code updated

--- src/api/store-data.ts
+++ src/api/store-data.ts
@@ -7,6 +7,10 @@ export function URL_StoreDetails(id: number) {
     return `api/v1/stores/${id}`;
 }
 
+export function URL_StoreCategoryDetails(store_id: number,menu_id: number){
+    return `api/v1/stores/${store_id}/menus/${menu_id}/overview`;
+}
+
 export function URL_StoreItemDetail(storeId: number, itemId: number) {
     return `api/customer/stores/${storeId}/${itemId}/modifiers`;
 }
