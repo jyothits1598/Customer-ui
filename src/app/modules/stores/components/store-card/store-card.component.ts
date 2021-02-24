e6f957ca370f6e1eaa4a50af8016934a9a819b20
Basavakumar <vishwanatha.st@sanradiance.com>
2021-02-24 19:22:42 +0530

    store search result, items screen

--- src/app/modules/stores/components/store-card/store-card.component.ts
+++ src/app/modules/stores/components/store-card/store-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ElementRef, Self } from '@angular/core';
+import { NgControl } from '@angular/forms';
 import { Store } from '../../model/store';
 
 @Component({
@@ -11,12 +12,17 @@ export class StoreCardComponent implements OnInit {
   @Input() showItems: boolean;
   @Output() selected = new EventEmitter<number>();
 
-  constructor() {}
+  constructor(
+    // @Self() public controlDir: NgControl,
+    private e: ElementRef
+    ) {
+  }
 
   static_image_path = "https://cdn.statically.io/img/"
   static_image_width = '?w=600,q=50,f=webp';
   static_image_width_logo ='?w=100,q=50,f=webp';
 
+  static_image_width_item = '?w=250,q=50,f=webp';
 
   ngOnInit(): void {}
 
@@ -24,5 +30,8 @@ export class StoreCardComponent implements OnInit {
     this.selected.next(id);
   }
 
+  showDecimal(number){
+    return Math.round((parseFloat(number) + Number.EPSILON) * 100) / 100
+  }
   
 }
